289143ea9c6cfd2bf6be958ee819be6d6bdfc095
RinkaladesaraTops <devp18320@gmail.com>
2024-09-09 11:02:31 +0530

    file added

---
+++ calc.js
@@ -0,0 +1,18 @@
+// parseInt()
+// let a = parseInt(prompt("Enter a:"))
+// let b = prompt("Enter b:")
+// let c = a+parseInt(b); //2510
+//parseFloat
+
+//Arithmetic operator
+// +,-,*,/,%(modulus),**(exponent),++(increment),--(decrement)
+
+// document.getElementById("demo").innerHTML = "Addition is "+c
+
+let ans = 7%3
+console.log("Reminder is ",ans);
+
+let r = 4;
+let result = r**3;
+document.write("Square is "+result)
+
---
+++ second.js
@@ -0,0 +1,22 @@
+document.getElementById("demo").innerHTML = "<b>JS File</b>"
+document.getElementById("txt").innerText = "<b>JS File</b>"
+
+//variable var,const,let
+//var - redeclare & reassign
+var a=10
+document.write("A is "+a)
+var a=25
+document.write("<br> A is "+a)
+
+//const -- constant - cant redeclare & reassign
+const pi=3.14
+document.write("<br> PI is "+pi)
+
+//let - cant redeclare but reassign
+let str = "Hii"
+document.write("<br> String is "+str)
+
+str = 67
+document.write("<br> String is "+str)
+
+
