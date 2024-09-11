87369f7a36cb35a3ee664cf4ab022576e9e3b5dc
vaibhavmulchandani21imca <vaibhavmulchandani.21.imca@iict.indusuni.ac.in>
2024-09-11 11:20:50 +0530

    ifelse

---
+++ myvar.js
@@ -0,0 +1,28 @@
+document.getElementById("demo").innerHTML = "My Second File"
+//to declare a variable - 3 ways - var,let,const
+//var - Redeclare & reassign
+// what is js?
+// why we can use js?
+//diff b/w let,var,const
+var a = 10
+var b = 25
+var c = a+b
+// operator --- -,*,/
+document.write("Addition is "+c)
+
+var c = b-a
+document.write("<br> Minus is "+c)
+
+//const - Can't redeclare & reassign
+const pi=3.14
+document.write("<br> PI is "+pi)
+
+// const pi=45
+// document.write("<br> PI is "+pi)
+
+//let - cant redeclare but reassign
+let str = "Hii"
+document.write("<br> String is "+str)
+
+str = 25
+document.write("<br> String is "+str)
